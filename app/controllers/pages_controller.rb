d5448b75d9881081b8aa815d3e4b2e5036ff64d8
Marko Anastasov <marko@renderedtext.com>
2023-06-15 12:33:22 +0200

    Show all content we have.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @categories = Category.all.order("name ASC")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 
-  resources :categories, path: '/', only: [] do
+  resources :categories, path: '/', only: [:show] do
     resources :kpis, path: '/', only: [:show]
   end
 
