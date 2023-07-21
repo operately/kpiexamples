71a8645ccd2a4971d4602bb29e67a9ddb3a693bf
Marko Anastasov <marko@renderedtext.com>
2023-07-21 14:20:42 +0200

    Scaffold sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/contribute', to: 'pages#contribute'
   get '/search', to: 'kpis#search'
+  get '/users/sign_in', to: 'pages#sign_in'
 
   resources :categories, path: '/', only: [:show] do
     resources :kpis, path: '/', only: [:show]
