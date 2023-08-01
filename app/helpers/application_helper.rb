f191b55d7c7e9de663ca09176620a241974c12b4
Marko Anastasov <marko@renderedtext.com>
2023-08-01 17:40:00 +0200

    Meta descriptions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     @page_title = title
     @page_title += " - KPI Examples" unless options[:full].present?
   end
+
+  def substring_until_period(string)
+    index = string.index('.')
+    index ? string[0...index+1] : string
+  end
+
+  def meta_description(content)
+    "<meta name=\"description\" content=\"#{content}\">".html_safe
+  end
 end
