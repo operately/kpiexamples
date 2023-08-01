afaf33d0b5df0a90046a88f03181f1b2282e5ae6
Marko Anastasov <marko@renderedtext.com>
2023-08-01 17:28:17 +0200

    Page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title
+    @page_title ||= "KPI Examples"
+  end
+
+  def set_page_title(title, options = {})
+    @page_title = title
+    @page_title += " - KPI Examples" unless options[:full].present?
+  end
 end
