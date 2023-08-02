1a82125c4c061b9a711893b4f16d820a3c4851fa
Marko Anastasov <marko@renderedtext.com>
2023-08-02 11:11:59 +0200

    Category view

--- app/helpers/kpis_helper.rb
+++ app/helpers/kpis_helper.rb
@@ -1,2 +1,16 @@
 module KpisHelper
+  def emoji_for_unit(unit_type)
+    case unit_type
+    when 'money'
+      "💰"
+    when 'percentage'
+      "%"
+    when 'time'
+      "⏱"
+    when 'number'
+      "#"
+    else
+      ""
+    end
+  end
 end
