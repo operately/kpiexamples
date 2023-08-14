5cfc9a45b32f0ea06ab0c1ba14945c9b0f24ed9f
Marko Anastasov <marko@renderedtext.com>
2023-08-14 17:01:31 +0200

    Update sheet name

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -3,7 +3,7 @@ class Sheet
   def initialize
     @session = GoogleDrive::Session.from_config("google_drive_config.json")
 
-    @spreadsheet = @session.spreadsheet_by_title("KPIs data model draft")
+    @spreadsheet = @session.spreadsheet_by_title("KPI Examples Data")
 
     @worksheet = @spreadsheet.worksheets.third
   end
