033eb9036282f144b0b3e1feb8efe18242ff456e
Marko Anastasov <marko@renderedtext.com>
2023-08-31 12:12:19 +0200

    Update email domain in config too

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey',
     :password => ENV['SENDGRID_API_KEY'],
-    :domain => 'operately.com',
+    :domain => 'kpiexamples.operately.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
