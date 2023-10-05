a214265c5e9ab56ae45758e2344251bd71425e99
Marko Anastasov <marko@renderedtext.com>
2023-10-05 17:40:41 +0200

    Most basic admin/comments list

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :notifications, only: [:index]
     resources :users, only: [:index, :show]
+    resources :comments, only: [:index, :show, :destroy]
     root to: 'dashboard#index'
   end
 
