8d0dff60ad7121a0ab053ca47d34e8ac4c84d159
Marko Anastasov <marko@renderedtext.com>
2023-10-06 13:26:19 +0200

    Give up dancing around Turbo, delay loading tinymce.

--- app/javascript/controllers/tinymce_controller.js
+++ app/javascript/controllers/tinymce_controller.js
@@ -1,10 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
 
-// This controller exists so that we avoid race conditions with Turbo.  We
-// don't need to explicitly handle Turbo events. When an element (textarea)
-// associated with this controller gets connected or disconnected from the
-// DOM, these methods will be automatically called, initializing or destroying
-// the TinyMCE instance as appropriate.
 export default class extends Controller {
   connect() {
     this.initEditor();
@@ -15,18 +10,20 @@ export default class extends Controller {
   }
 
   initEditor() {
-    if (window.tinymce) {
-      tinymce.init({
-        target: this.element,
-        toolbar: [ 'styleselect | bold italic | bullist numlist | link | undo redo' ],
-        plugins: 'lists link',
-        menubar: false,
-        statusbar: false,
-        height: 220,
-        default_link_target: '_blank',
-        link_assume_external_targets: 'https'
-      });
-    }
+    setTimeout(() => { // ugly but it works
+      if (window.tinymce) {
+        tinymce.init({
+          target: this.element,
+          toolbar: [ 'styleselect | bold italic | bullist numlist | link | undo redo' ],
+          plugins: 'lists link',
+          menubar: false,
+          statusbar: false,
+          height: 220,
+          default_link_target: '_blank',
+          link_assume_external_targets: 'https'
+        });
+      }
+    }, 500);
   }
 
   destroyEditor() {
