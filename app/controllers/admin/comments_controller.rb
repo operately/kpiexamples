5edfec49f2a5153ae927fe46f56a38cf8bc2df55
Marko Anastasov <marko@renderedtext.com>
2023-10-06 11:58:21 +0200

    Admin can view and delete a comment

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,4 +2,17 @@ class Admin::CommentsController < Admin::BaseController
   def index
     @comments = Comment.order("created_at DESC")
   end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to admin_comments_path, notice: "Comment deleted."
+    else
+      redirect_to [:admin, @comment], notice: "Failed to delete comment."
+    end
+  end
 end
