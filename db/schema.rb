38cacd17f85430bcd5cb29e1a2c5cdb90312f655
Marko Anastasov <marko@renderedtext.com>
2023-11-20 12:47:58 +0100

    Avoid N+1 query on homepage by introducing Category#pending.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_091727) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_114558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_091727) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.text "summary"
+    t.boolean "pending", default: true
     t.index ["slug"], name: "index_categories_on_slug", unique: true
   end
 
